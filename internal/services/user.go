469d1fe8c19b67669fb4d3de8123b3d6c9de531c
Vasilii Safronov <vasi1988@mail.ru>
2024-07-09 00:27:44 +0300

    refactoring user service

--- internal/services/user.go
+++ internal/services/user.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/CatLecter/gin_template/internal/repositories"
 	"github.com/CatLecter/gin_template/internal/schemes"
-	"github.com/CatLecter/gin_template/utils"
+	"github.com/CatLecter/gin_template/internal/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
@@ -19,100 +19,109 @@ func NewUserService(repo repositories.UserRepositoryInterface) UserServiceInterf
 }
 
 func (srv *UserService) GetUserByUUID(ctx *fiber.Ctx) error {
-	userUUID, err := uuid.Parse(ctx.Query("uuid"))
-	if err != nil {
-		log.Errorf("Error parsing user UUID: %v", err.Error())
+	var err error
+	var userUUID uuid.UUID
+
+	if userUUID, err = uuid.Parse(ctx.Query("uuid")); err != nil {
+		log.Warnf("Error parsing user UUID: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("cannot parse user UUID"))
 	}
-	userResp, err := srv.repo.GetUserByUUID(&userUUID)
-	if err != nil {
+
+	if userResp, err := srv.repo.GetUserByUUID(&userUUID); err != nil {
 		return ctx.Status(fiber.StatusNotFound).JSON(
 			utils.NewHTTPError(fmt.Sprintf("user with UUID=%v not found", userUUID)),
 		)
+	} else {
+		return ctx.Status(fiber.StatusOK).JSON(userResp)
 	}
-	return ctx.Status(fiber.StatusOK).JSON(userResp)
 }
 
 func (srv *UserService) CreateUser(ctx *fiber.Ctx) error {
-	user := schemes.UserRequest{}
-	if err := ctx.BodyParser(&user); err != nil {
+	newUser := new(schemes.UserRequest)
+
+	if err := ctx.BodyParser(&newUser); err != nil {
 		log.Warnf("Error parsing body: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("cannot parse JSON"))
 	}
-	isExists, err := srv.repo.CheckUserByPhone(&user.Phone)
-	if err != nil {
+
+	if isExists, err := srv.repo.CheckUserByPhone(&newUser.Phone); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("error checking the user's existence"))
-	}
-	if *isExists {
+	} else if *isExists {
 		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(
-			utils.NewHTTPError(fmt.Sprintf("user with phone number %v already exists", user.Phone)),
+			utils.NewHTTPError(fmt.Sprintf("user with phone number %v already exists", newUser.Phone)),
 		)
 	}
-	userResp, err := srv.repo.CreateUser(&user)
-	if err != nil {
+
+	if userResp, err := srv.repo.CreateUser(newUser); err != nil {
 		log.Warnf("Error when creating a user: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("user cannot be created"))
+	} else {
+		return ctx.Status(fiber.StatusOK).JSON(userResp)
 	}
-	return ctx.Status(fiber.StatusOK).JSON(userResp)
 }
 
 func (srv *UserService) UpdateUserByUUID(ctx *fiber.Ctx) error {
-	userUUID, err := uuid.Parse(ctx.Query("uuid"))
-	if err != nil {
+	var err error
+	var userUUID uuid.UUID
+
+	if userUUID, err = uuid.Parse(ctx.Query("uuid")); err != nil {
 		log.Warnf("Error parsing user UUID: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("cannot parse user UUID"))
 	}
-	isExistsUser, err := srv.repo.CheckUserByUUID(&userUUID)
-	if err != nil {
+
+	if isExistsUser, err := srv.repo.CheckUserByUUID(&userUUID); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("error checking the user's existence"))
-	}
-	if !*isExistsUser {
+	} else if !*isExistsUser {
 		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(
 			utils.NewHTTPError(fmt.Sprintf("user with uuid %v not found", userUUID)),
 		)
 	}
-	user := schemes.UserRequest{}
-	if err := ctx.BodyParser(&user); err != nil {
+
+	newUserData := new(schemes.UserRequest)
+
+	if err := ctx.BodyParser(&newUserData); err != nil {
 		log.Warnf("Error parsing body: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("cannot parse JSON"))
 	}
-	isExistsPhone, err := srv.repo.CheckUserByPhone(&user.Phone)
-	if err != nil {
+
+	if isExistsPhone, err := srv.repo.CheckUserByPhone(&newUserData.Phone); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("error checking the user's existence"))
-	}
-	if *isExistsPhone {
+	} else if *isExistsPhone {
 		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(
-			utils.NewHTTPError(fmt.Sprintf("user with phone number %v already exists", user.Phone)),
+			utils.NewHTTPError(fmt.Sprintf("user with phone number %v already exists", newUserData.Phone)),
 		)
 	}
-	responseUser, err := srv.repo.UpdateUserByUUID(&userUUID, &user)
-	if err != nil {
+
+	if userResp, err := srv.repo.UpdateUserByUUID(&userUUID, newUserData); err != nil {
 		log.Warnf("Error when updating a user: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("user cannot be updated"))
+	} else {
+		return ctx.Status(fiber.StatusOK).JSON(userResp)
 	}
-	return ctx.Status(fiber.StatusOK).JSON(responseUser)
 }
 
 func (srv *UserService) DeleteUserByUUID(ctx *fiber.Ctx) error {
-	userUUID, err := uuid.Parse(ctx.Query("uuid"))
-	if err != nil {
+	var err error
+	var userUUID uuid.UUID
+
+	if userUUID, err = uuid.Parse(ctx.Query("uuid")); err != nil {
 		log.Warnf("Error parsing user UUID: %v", err.Error())
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("cannot parse user UUID"))
 	}
-	isExistsUser, err := srv.repo.CheckUserByUUID(&userUUID)
-	if err != nil {
+
+	if isExistsUser, err := srv.repo.CheckUserByUUID(&userUUID); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.NewHTTPError("error checking the user's existence"))
-	}
-	if !*isExistsUser {
+	} else if !*isExistsUser {
 		return ctx.Status(fiber.StatusNotFound).JSON(
 			utils.NewHTTPError(fmt.Sprintf("user with uuid %v not found", userUUID)),
 		)
 	}
-	err = srv.repo.DeleteUserByUUID(&userUUID)
-	if err != nil {
+
+	if err = srv.repo.DeleteUserByUUID(&userUUID); err != nil {
 		return ctx.Status(fiber.StatusNotFound).JSON(
 			utils.NewHTTPError(fmt.Sprintf("user with an UUID=%v cannot be deleted", userUUID)),
 		)
 	}
+
 	return ctx.Status(fiber.StatusOK).JSON(schemes.HTTPResponse{Result: "success", Msg: "user deleted successfully"})
 }
